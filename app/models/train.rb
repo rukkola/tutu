741c1e867e89b452b1a34ba02c9ed4c8097bf335
Serg <seregaproger@yandex.ru>
2021-05-13 15:09:10 +0300

    train has_many :carriages

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,6 +1,7 @@
 class Train < ApplicationRecord
 	validates :number, presence: true 
-
+  
 	#belongs_to :railway_station
 	belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id # имя ассоциации(модель RailwayStation)
+  has_many :carriages
 end
