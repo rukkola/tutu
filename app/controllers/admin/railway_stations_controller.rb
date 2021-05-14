a497ac65fedc02477c0919dcf58e931368f0d150
Serg <seregaproger@yandex.ru>
2021-05-14 23:40:20 +0300

    add "admin" in routes/show and railway_stations_controller

--- app/controllers/admin/railway_stations_controller.rb
+++ app/controllers/admin/railway_stations_controller.rb
@@ -71,7 +71,7 @@ class Admin::RailwayStationsController < Admin::BaseController
   def update_position
     @route = Route.find(params[:route_id])
     @railway_station.update_position(@route, params[:position])
-    redirect_to @route
+    redirect_to [:admin, @route]
   end
 
   private
