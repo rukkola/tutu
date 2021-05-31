84e01e6dc371d2d010b5a2f3f5bb93ced88b5e25
Serg <seregaproger@yandex.ru>
2021-06-01 00:17:31 +0300

    added in search: root_start_station, root_end_station

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,18 +1,26 @@
 class Route < ApplicationRecord
-	#has_and_belongs_to_many :railway_stations #название ассоциации railway_stations
-	# Есть третья таблица(_ailway_stations_routes) и через нее мы получаем доступ к (ailway_stations)
+	
 	has_many :railway_stations_routes
 	has_many :railway_stations, through: :railway_stations_routes
 
-  before_validation :set_name
+  #before_validation :set_name
 	validates :name, presence: true
 	validate :stations_count
+ 
+	def root_start_station(route)
+    route.railway_stations_routes.order('position').first.railway_station_id
+  end
+
+  def root_end_station(route)
+    route.railway_stations_routes.order('position DESC').first.railway_station_id
+  end 
 
 	private
-
-	def set_name
-		self.name = "#{railway_stations.first.title} - #{railway_stations.last.title}"
-	end
+  
+  # переписать с использованием "position".
+	#def set_name
+	#	self.name = "#{railway_stations.first.title} - #{railway_stations.last.title}"
+	#end
 
 	def stations_count
 		if railway_stations.size < 2
@@ -20,5 +28,4 @@ class Route < ApplicationRecord
 			errors.add(:base, "Маршрут должен содержать минимум 2 станции")
 		end
 	end
-
 end
