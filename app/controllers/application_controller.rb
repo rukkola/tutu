746a754242da3250f3ec45f869ca590e08ee0ff9
Serg <seregaproger@yandex.ru>
2021-06-08 23:27:51 +0300

    added Time Zones

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  around_action :user_time_zone, if: :current_user
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:time_zone])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:time_zone])
+  end
+
+  private
+
+  def user_time_zone(&block)
+    Time.use_zone(current_user.time_zone, &block)
+  end
+
 end
